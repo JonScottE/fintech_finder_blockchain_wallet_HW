496eacff95b8a7d88472b14fb75a9e960004b503
JonScottE <91569353+JonScottE@users.noreply.github.com>
2022-03-28 13:21:05 -0500

    Add files via upload

--- fintech_finder.py
+++ fintech_finder.py
@@ -148,7 +148,7 @@ st.sidebar.write(account.address)
 # @TODO : DONE
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-st.sidebar.write(get_balance(account.address))
+st.sidebar.write(get_balance(w3,account.address))
 
 ##########################################
 
@@ -270,8 +270,8 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    transaction_hash = send_transaction(account, candidate_address, wage)
-
+    transaction_hash = send_transaction(w3, account, candidate_address, wage)
+    
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
 
